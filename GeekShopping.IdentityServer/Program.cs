32c4c5d65d861289a6a0258446a4af573036c632
Rafael Menezes <rafa.rcm@hotmail.com>
2024-05-14 07:11:40 -0300

    Adicionando Configurações de Banco de Dados e Segurança

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,36 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.
+                                   UseMySql(connection,
+                                   new MySqlServerVersion(
+                                   new Version(8, 0, 2))));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderServices = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+  .AddInMemoryClients(IdentityConfiguration.Clients)
+  .AddAspNetIdentity<ApplicationUser>();
+
+builderServices.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -13,6 +42,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
