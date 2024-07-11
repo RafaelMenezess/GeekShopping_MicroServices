36c31d8ba06deef65232c247df9923c707121b71
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-11 10:06:31 -0300

    Criando o Método Responsável por Buscar o Carrinho de Compras do Usuário

--- GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.Web.Controllers;
@@ -15,6 +17,23 @@ public class CartController : Controller
     }
     public async Task<IActionResult> CartIndex()
     {
-        return View();
+        return View(await FindUserCart());
+    }
+
+    private async Task<CartViewModel> FindUserCart()
+    {
+        var token = await HttpContext.GetTokenAsync("access_token");
+        var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+        var response = await _cartService.FindCartByUserId(userId, token);
+        if (response?.CartHeader != null)
+        {
+            foreach (var detail in response.CartDetails)
+            {
+                response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+            }
+        }
+
+        return response;
     }
 }
