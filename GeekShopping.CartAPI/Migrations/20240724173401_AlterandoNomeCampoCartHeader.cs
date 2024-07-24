238f7b87376482b772abff3bfdf0f8b920b847e4
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-24 14:35:29 -0300

    Correção campo "coupon_code" cart_header

---
+++ GeekShopping.CartAPI/Migrations/20240724173401_AlterandoNomeCampoCartHeader.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.CartAPI.Migrations
+{
+    public partial class AlterandoNomeCampoCartHeader : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "coupun_code",
+                table: "cart_header",
+                newName: "coupon_code");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "coupon_code",
+                table: "cart_header",
+                newName: "coupun_code");
+        }
+    }
+}
--- GeekShopping.CartAPI/Model/CartHeader.cs
+++ GeekShopping.CartAPI/Model/CartHeader.cs
@@ -8,6 +8,6 @@ public class CartHeader : BaseEntity
 {
     [Column("user_id")]
     public string UserId { get; set; }
-    [Column("coupun_code")]
+    [Column("coupon_code")]
     public string CouponCode { get; set; }
 }
