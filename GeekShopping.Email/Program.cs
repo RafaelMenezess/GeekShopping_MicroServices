ba935cd6ec67527bc7cd895c809db4f78df3f7f5
Rafael Menezes <rafa.rcm@hotmail.com>
2024-08-08 14:36:11 -0300

    Implementando os Repositórios

--- GeekShopping.Email/Program.cs
+++ GeekShopping.Email/Program.cs
@@ -1,4 +1,6 @@
+using GeekShopping.Email.MessageConsumer;
 using GeekShopping.Email.Model.Context;
+using GeekShopping.Email.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +11,16 @@ builder.Services.AddDbContext<MySQLContext>(options => options.
                                    UseMySql(connection,
                                    new MySqlServerVersion(
                                    new Version(8, 0, 2))));
+
+var builderDb = new DbContextOptionsBuilder<MySQLContext>();
+builderDb.UseMySql(connection,
+                new MySqlServerVersion(
+                new Version(8, 0, 2)));
+
+builder.Services.AddSingleton(new EmailRepository(builderDb.Options));
+builder.Services.AddScoped<IEmailRepository, EmailRepository>();
+
+builder.Services.AddHostedService<RabbitMQPaymentConsumer>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,28 +38,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
